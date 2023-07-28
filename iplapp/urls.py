f0098f2bab31ece364ba9ad0861b93bb76dd6d11
Sanjeeva Reddy <31734152+sanjeevareddy91@users.noreply.github.com>
2023-07-28 20:38:33 +0530

    Update urls.py

--- iplapp/urls.py
+++ iplapp/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('update_team/<id>',views.update_team,name="update_team"),
     path('delete_team/<id>',views.delete_team,name="delete_team"),
     path('modelform/',views.team_modelform,name="model_form"),
+    path('normalform/',views.team_info_form,name="normal_form")
     path('registeruser/',views.register_user,name="register_user")
 ]
